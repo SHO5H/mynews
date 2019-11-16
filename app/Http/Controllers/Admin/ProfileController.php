2d7455368b716ccbccebec44fc01ba722a2534ce
SHOHEI <sho5h0311@yahoo.co.jp>
2019-11-16 02:58:06 +0000

    15PHP

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,26 +5,74 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class ProfileController extends Controller
+use App\Profile;
+
+
+class NewsController extends Controller
 {
-    //
-     public function add()
-    {
-        return view('admin.profile.create');
-    }
+  public function add()
+  {
+      return view('admin.profile.create');
+  }
 
-    public function create()
-    {
-        return redirect('admin/profile/create');
-    }
+  public function create(Request $request)
+  {
 
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
 
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+      $news = new Profile;
+      $form = $request->all();
+
+      // formに画像があれば、保存する
+      if ($form['image']) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+
+      unset($form['_token']);
+      unset($form['image']);
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
+
+      return redirect('admin/profile/create');
+  }
+
+  public function edit(Request $request)
+  {
+      // News Modelからデータを取得する
+      $news = Profile::find($request->id);
+      if (empty($news)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['news_form' => $news]);
+  }
+
+
+  public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $news =   Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $news_form = $request->all();
+      unset($news_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $news->fill($news_form)->save();
+
+      return redirect('admin/profile');
+  }
+  public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $news = Profile::find($request->id);
+      // 削除する
+      $news->delete();
+      return redirect('admin/profile/');
+  }  
 }
--- app/Profile.php
+++ app/Profile.php
@@ -4,10 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class News extends Model
+class Profile extends Model
 {
     protected $guarded = array('id');
-
     public static $rules = array(
         'name' => 'required',
         'gender' => 'required',
